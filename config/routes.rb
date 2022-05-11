f35fd256d72b241c1c15c36418b320fcbc9d039a
Enoma Victor <vickjunior2002@gmail.com>
2022-05-11 21:20:28 +0100

    add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :pages
   resources :articles
