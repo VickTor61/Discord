cec86835517d80039d7ea941daaba1bca9785a44
Enoma Victor <vickjunior2002@gmail.com>
2022-05-18 12:42:11 +0100

    create admin user and give admin privileges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_current_user, only: [:edit, :update]
+  before_action :require_current_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
+  
   def index
     @pagy, @users = pagy(User.all, items: 5)
   end
@@ -16,23 +18,31 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+
       flash[:success] = "Welcome to the alpha blog #{ @user.username }"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render :new
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
-      flash[:success] = "User was successfully updated"
+      flash[:success] = 'User was successfully updated'
       redirect_to articles_path
     else
       render :edit
     end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "Sucessfully deleted user and all user articles"
+    redirect_to users_path
   end
 
 
@@ -46,9 +56,16 @@ class UsersController < ApplicationController
   end
 
   def require_current_user
-    if current_user != @user
-      flash[:danger] = "You can only edit your account"
+    if current_user != @user && !current_user.admin?
+      flash[:danger] = 'You can only edit your account'
       redirect_to root_path
+    end
+  end
+  
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = 'Only admin users can perform that action'
+      redirect root_path
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_230958) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_103413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_230958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
